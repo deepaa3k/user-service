f078007763fde5079844694c477849a978af7ef2
Your Name <you@example.com>
2022-04-14 11:55:29 -0700

    commit user changes

---
+++ Module6-MicroService-Security/src/main/java/com/edu/Module6MicroServiceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module6MicroServiceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module6MicroServiceSecurityApplication.class, args);
+	}
+
+}
